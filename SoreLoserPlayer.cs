85f057a5e217a5b127a504844ddec6a2267ab8dd
Curtis Webb <curtwebb1194@@gmail.com>
2023-06-06 13:53:40 -0500

    updated code

--- SoreLoserPlayer.cs
+++ SoreLoserPlayer.cs
@@ -1,11 +1,24 @@
+using System;
+
 namespace ShootingDice
 {
-    // TODO: Complete this class
-
     // A Player that throws an exception when they lose to the other player
     // Where might you catch this exception????
-    public class SoreLoserPlayer
+    public class SoreLoserPlayer : Player
     {
-        
+        public override void Play(Player other)
+        {
+            int myRoll = Roll();
+            int otherRoll = other.Roll();
+
+            if (myRoll <= otherRoll)
+            {
+                throw new Exception("I won't lose!");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} wins!");
+            }
+        }
     }
 }
--- SoreLoserUpperHalfPlayer.cs
+++ SoreLoserUpperHalfPlayer.cs
@@ -1,11 +1,24 @@
+using System;
+
 namespace ShootingDice
 {
-    // TODO: Complete this class
-
     // A Player who always rolls in the upper half of their possible role and
-    //  who throws an exception when they lose to the other player
-    public class SoreLoserUpperHalfPlayer
+    // who throws an exception when they lose to the other player
+    public class SoreLoserUpperHalfPlayer : UpperHalfPlayer
     {
+        public override void Play(Player other)
+        {
+            int myRoll = Roll();
+            int otherRoll = other.Roll();
 
+            if (myRoll <= otherRoll)
+            {
+                throw new Exception("I won't lose!");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} wins!");
+            }
+        }
     }
 }
